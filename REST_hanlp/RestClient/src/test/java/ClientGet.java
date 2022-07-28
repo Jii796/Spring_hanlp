f0ec279ead6eb04486255bc6631da2cbec585eec
Jii796 <83084341+Jii796@users.noreply.github.com>
2022-07-28 17:09:19 +0800

    Add files via upload

---
+++ REST_hanlp/RestClient/src/test/java/ClientGet.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
+public class ClientGet {
+    /**
+     * get请求
+     * @throws UnsupportedEncodingException
+     */
+    @Test
+    public void restTemplateGetTest() throws UnsupportedEncodingException {
+        String text="东川区应急管理局截至目前未接到安全生产类和自然灾害类的情况报告。";
+        //text= URLEncoder.encode(text,"UTF-8");
+        RestTemplate restTemplate = new RestTemplate();
+        String string = restTemplate.getForObject("http://172.18.8.43:18101/textClassify/{1}"
+                , String.class,text);
+        System.out.println(URLDecoder.decode(string,"UTF-8"));
+    }
+}
