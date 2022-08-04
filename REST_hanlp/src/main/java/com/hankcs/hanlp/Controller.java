955c38f24a3e314a8048a42dcbfe825132e52ad8
Jii796 <83084341+Jii796@users.noreply.github.com>
2022-08-04 15:23:57 +0800

    Add files via upload

---
+++ REST_hanlp/src/main/java/com/hankcs/hanlp/Controller.java
@@ -0,0 +1,193 @@
+package com.hankcs.hanlp;
+
+import com.hankcs.hanlp.suggest.Suggester;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.net.URLDecoder;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+
+/**
+ * @author CAKJ
+ */
+@RestController
+@SpringBootApplication
+public class Controller {
+
+    private static final String[] TYPE = {"first-type","second-type","third-type","forth-type","fifth-type","sixth-type"};
+    private static final String[] TYPE_POSSIBILITY= {"first-type-possibility","second-type-possibility","third-type-possibility",
+        "forth-type-possibility","fifth-type-possibility","sixth-type-possibility"};
+    private static Suggester suggester;
+
+    private static final String UNKNOWN="unknown";
+    private static final String ERROR="error";
+
+    private static final String ACCESSTOKEN="TEXTCLASSIFYFORHANLP";
+    private static final Logger logger = Logger.getLogger(Controller.class.getName());
+    /**
+     * POST请求
+     * @param request 这是接收请求报文
+     * @param text 这是接受到的参数
+     * @param accessToken 接收到的参数
+     * @return 返回一个map类型的结果，存放类型和类型的可能性
+     * @throws IOException 抛出异常
+     */
+    @PostMapping(value="/textClassify")
+    public Map<String,String> textClassifyControllerPost(HttpServletRequest request,String text,String accessToken) throws IOException {
+
+        Map<String,String> map=new HashMap<>();
+        if(text!=null&&accessToken.equals(ACCESSTOKEN)){
+            map.put("Token","OK");
+            text=URLDecoder.decode(text,"UTF-8");
+            getRequestHeaders(request);
+            Map<String,Map> textMap= TextClassify.textClassifyController(text);
+            String[] re= (String[]) textMap.get("map1").get("label");
+            Double[] re2=(Double[])textMap.get("map2").get("possibility");
+            logger.info(text);
+            int i=size(re);
+            //MultiValueMap map2 = new LinkedMultiValueMap();这个是之前使用的返回类型，但是在客户端获取的时候会出现中括号，因此改为了直接使用Map
+            map.put(TYPE[0],stringToInt(re[0]));
+            map.put(TYPE_POSSIBILITY[0],re2[0].toString());
+            for(int j=1;j<i;j++){
+                map.put(TYPE[j],stringToInt(re[j]));
+                map.put(TYPE_POSSIBILITY[j],re2[j].toString());
+            }
+        }else if(accessToken==null){
+            map.put("Token","null");
+        }else if(!accessToken.equals(ACCESSTOKEN)){
+            map.put("Token",ERROR);
+        }else{
+            map.put("status",ERROR);
+        }
+        return map;
+    }
+
+    /**
+     * get
+     * @param request 接受到的请求报文
+     * @param text 接受到的参数
+     * @param accessToken 接收到的参数
+     * @return 返回一个map类型，存放的是类型和类型的可能性
+     * @throws IOException 抛出异常
+     */
+    @ResponseBody
+    @GetMapping(path="/textClassify/{text}/{accessToken}")
+    public Map<String,String> textClassifyControllerGet(HttpServletRequest request,
+                                                        @PathVariable(value = "text") String text,
+                                                        @PathVariable(value="accessToken") String accessToken) throws IOException {
+        Map<String,String> map=new HashMap<>();
+        if(text!=null&&accessToken.equals(ACCESSTOKEN)){
+            getRequestHeaders(request);
+            Map<String,Map> textMap= TextClassify.textClassifyController(text);
+            String[] re= (String[]) textMap.get("map1").get("label");
+            Double[] re2=(Double[]) textMap.get("map2").get("possibility");
+            logger.info(text);
+            int i=size(re);
+            map.put(TYPE[0],stringToInt(re[0]));
+            map.put(TYPE_POSSIBILITY[0],re2[0].toString());
+            for(int j=1;j<i;j++){
+                map.put(TYPE[j],stringToInt(re[j]));
+                map.put(TYPE_POSSIBILITY[j],re2[j].toString());
+            }
+        }else if(accessToken==null){
+            map.put("Token","null");
+        }else if(!accessToken.equals(ACCESSTOKEN)){
+            map.put("Token",ERROR);
+        }else{
+            map.put("status",ERROR);
+        }
+        return map;
+    }
+
+    /**
+     * 通过该方法获得输入文本的推荐文本
+     * get
+     * @param keyword 输入的参数
+     * @return 返回一个map，存放的是关键字以及和关键字匹配的一段文本
+     */
+    @GetMapping(path="/textSuggest/{text}")
+    public Map<String,String> textSuggestController(@PathVariable(value="text") String keyword) {
+        Map<String,String> map=new HashMap<>();
+        String suggestResult=suggester.suggest(keyword,1).get(0);
+        map.put("keyword",keyword);
+        map.put("similar text",suggestResult);
+        return map;
+    }
+    /**
+     * 该方法的功能为获取一个String类型的数组中实际元素的个数
+     * @param list 输入的参数列表
+     * @return 返回列表的实际元素的个数
+     */
+    public int size(String[] list){
+        int length=0;
+        for (String s : list) {
+            if (s != null) {
+                length += 1;
+            }
+        }
+        return length;
+    }
+    public int size(Double[] list){
+        int length=0;
+        for (Double aDouble : list) {
+            if (aDouble != null) {
+                length += 1;
+            }
+        }
+        return length;
+    }
+    public static String stringToInt(String type){
+        switch(type) {
+            case "安全生产":
+                return "0001";
+            case "社会安全":
+                return "0002";
+            case "自然灾害":
+                return "0003";
+            case "公共卫生":
+                return "0004";
+            case "通知":
+                return "0005";
+            case "其他":
+                return "0006";
+            default:
+                return "0000";
+        }
+    }
+
+
+    /**
+     *
+     * @param request 获得的请求报文头部
+     */
+    private static void getRequestHeaders(HttpServletRequest request){
+        logger.info("Request Headers:");
+        logger.info(request.getMethod());
+        logger.info("IP:"+getIpAddr(request));
+        logger.info("Host:"+request.getRemoteHost());
+        logger.info("Port:"+request.getRemotePort());
+        logger.info("Referer:"+request.getRequestURL());
+        logger.info("Content-Type:"+request.getContentType());
+    }
+    public static String getIpAddr(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if(ip == null || ip.length() == 0 || UNKNOWN.equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if(ip == null || ip.length() == 0 || UNKNOWN.equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if(ip == null || ip.length() == 0 || UNKNOWN.equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
+    }
+    public static void main(String[] args){
+        SpringApplication.run(Controller.class, args);
+    }
+}
---
+++ REST_hanlp/src/main/java/com/hankcs/hanlp/TextClassify.java
@@ -0,0 +1,153 @@
+package com.hankcs.hanlp;
+
+import com.hankcs.hanlp.classification.classifiers.IClassifier;
+import com.hankcs.hanlp.classification.classifiers.NaiveBayesClassifier;
+import com.hankcs.hanlp.classification.models.NaiveBayesModel;
+import com.hankcs.hanlp.corpus.io.IOUtil;
+import com.hankcs.hanlp.utility.TestUtility;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+
+/**
+ * 该类用来进行文本分类，其中包括TextClassifyController和predictController两个方法为服务器端调用
+ *
+ * @author hankcs
+ */
+public class TextClassify
+{
+    private static final Logger logger = Logger.getLogger(TextClassify.class.getName());
+    /**
+     * 搜狗文本分类语料库5个类目，每个类目下1000篇文章，共计5000篇文章
+     */
+    public static final String CORPUS_FOLDER = TestUtility.ensureTestData("hanlp_rest_data-master", "https://codeload.github.com/Jii796/hanlp_rest_data/zip/refs/heads/master");
+    /**
+     * 模型保存路径
+     */
+    public static final String MODEL_PATH = "data/test/classification-model_for.ser";
+
+
+    public static void main(String[] args) throws IOException
+    {
+        IClassifier classifier = new NaiveBayesClassifier(trainOrLoadModel());
+
+        String[] filepath=getFilePath("YOUR_PATH");
+        for(int i=0;i<size(filepath);i++){
+            predict(classifier,getContext(filepath[i]));
+        }
+        predict(classifier, "某有限公司一名工人在作业工程中不幸遇难");
+        predict(classifier,"东川区应急管理局截至目前未接到安全生产类和自然灾害类的情况报告。");
+        predict(classifier,"安全生产");
+        Map<String,Map> result=classifier.classify("安全生产",3,true,1);
+        Double[] r1= (Double[]) result.get("map2").get("possibility");
+        logger.info(String.valueOf(size(r1)));
+    }
+
+    public static Map<String,Map> textClassifyController(String text) throws IOException {
+        IClassifier classifier =new NaiveBayesClassifier(trainOrLoadModel());
+        return predictController(classifier,text);
+    }
+    public static Map<String, Map> predictController(IClassifier classifier, String text){
+        return classifier.classify(text,3,true,1);
+    }
+    private static void predict(IClassifier classifier, String text)
+    {
+        String[] result=classifier.classify(text,3,true);
+        logger.info("《%s》 属于分类 \n【"+text);
+        for(int i=0;i< size(result);i++){
+            logger.info("%s,"+result[i]);
+        }
+        logger.info("】\n");
+    }
+    private static int size(String[] list){
+        int result=0;
+        for(int i=0;i<list.length;i++){
+            if(list[i]!=null){
+                result+=1;
+            }
+        }
+        return result;
+    }
+    private static int size(Double[] list){
+        int result=0;
+        for(int i=0;i<list.length;i++){
+            if(list[i]!=null){
+                result+=1;
+            }
+        }
+        return result;
+    }
+    public static String[] getFilePath(String path) {
+        String[] result = new String[20];
+        File file = new File(path);
+        if (file.isDirectory()) {
+            logger.info("文件夹");
+            String[] filelist = file.list();
+            for (int i = 0; i < filelist.length; i++) {
+                result[i]=path+"\\"+filelist[i];
+            }
+        }
+        return result;
+    }
+    public static String getContext(String path){
+        List<String> list = new ArrayList<>();
+        String encoding = "utf-8";
+        try
+        {
+            File file = new File(path);
+            if (file.isFile() && file.exists())
+            { // 判断文件是否存在
+                try (InputStreamReader read = new InputStreamReader(
+                    Files.newInputStream(file.toPath()), encoding)) {
+                    // 考虑到编码格式
+                    BufferedReader bufferedReader = new BufferedReader(read);
+                    String lineTxt = null;
+
+                    while ((lineTxt = bufferedReader.readLine()) != null) {
+                        list.add(lineTxt);
+                    }
+                    bufferedReader.close();
+                }
+            }
+            else
+            {
+                logger.info("找不到指定的文件");
+            }
+        }
+        catch (Exception e)
+        {
+            logger.info("读取文件内容出错");
+            e.printStackTrace();
+        }
+
+        return list.toString();
+
+    }
+    private static NaiveBayesModel trainOrLoadModel() throws IOException
+    {
+        NaiveBayesModel model = (NaiveBayesModel) IOUtil.readObjectFrom(MODEL_PATH);
+        if (model != null) {
+            return model;
+        }
+
+
+        File corpusFolder = new File(CORPUS_FOLDER);
+        if (!corpusFolder.exists() || !corpusFolder.isDirectory())
+        {
+            logger.info("没有文本分类语料，请阅读IClassifier.train(java.lang.String)中定义的语料格式与语料下载：" +
+                "https://github.com/hankcs/HanLP/wiki/%E6%96%87%E6%9C%AC%E5%88%86%E7%B1%BB%E4%B8%8E%E6%83%85%E6%84%9F%E5%88%86%E6%9E%90");
+            System.exit(1);
+        }
+         // 创建分类器，更高级的功能请参考IClassifier的接口定义
+        IClassifier classifier = new NaiveBayesClassifier();
+        // 训练后的模型支持持久化，下次就不必训练了
+        classifier.train(CORPUS_FOLDER);
+        model = (NaiveBayesModel) classifier.getModel();
+        IOUtil.saveObjectTo(model, MODEL_PATH);
+        return model;
+    }
+}
